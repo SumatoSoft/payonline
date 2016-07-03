db011ce5ecb4b32b67575df49c65a5e452d829a3
Dan K <itsdanya@gmail.com>
2016-07-03 16:12:09 +0300

    Payment response: rename RESPONSE_PARAMS to SIGNED_PARAMS

--- lib/payonline/payment_response.rb
+++ lib/payonline/payment_response.rb
@@ -4,9 +4,9 @@ module Payonline
 
     attr_accessor :data, :params
 
-    RESPONSE_PARAMS = %i(date_time transaction_id order_id amount currency)
+    SIGNED_PARAMS = %i(date_time transaction_id order_id amount currency)
 
-    def_delegators :data, *RESPONSE_PARAMS
+    def_delegators :data, *SIGNED_PARAMS
 
     def initialize(params = {})
       @params = prepare_params(params)
@@ -14,7 +14,7 @@ module Payonline
     end
 
     def valid_payment?
-      keys = RESPONSE_PARAMS.select { |key| @params.has_key?(key) }
+      keys = SIGNED_PARAMS.select { |key| @params.has_key?(key) }
       @params[:security_key] == Payonline::Signature.new(@params, keys, false).digest
     end
 
